e0277f3a8007beda523b8592e14054ba450bb25e
Muskhab Israilov <93041805+Muskhab@users.noreply.github.com>
2022-03-30 17:45:14 +0300

    Добавил проверку авторизации

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = async (req, res) => {
+    const {authorization} = req.headers
+
+    if(!authorization) {
+       return res.status(401).json({error: 'Ошибка авторизации(отстутвует headers)'})
+    }
+    const [type, token] = authorization.split(' ')
+    if (type !== 'Bearer') {
+        return res.status(401).json({error: "Ошибка авторизации(неверный тип Bearer)"})
+    }
+    try {
+        req.user = await jwt.verify(token, process.env.SECRET_JWT_KEY)
+    } catch (e) {
+        res.status(401).json({error: "Неверный токен: " + e.toString()})
+    }
+}
